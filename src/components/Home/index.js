ad631aa73510c41d328e952e8f9820f2289ce8b1
Gustavo Henrique <gugahribeiro@hotmail.com>
2020-06-23 12:18:09 -0300

    add component Home

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './styles.scss';
+
+class Home extends Component{
+
+    render(){
+        return(
+            <div className='home'>
+                <h1>Olá mundo</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
