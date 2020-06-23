3b0706a7db044ce7a8e307fff5d2fcb822aa3d0e
Gustavo Henrique <gugahribeiro@hotmail.com>
2020-06-23 12:14:20 -0300

    fix routes

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Cronometro from './components/Cronometro';
 import Relogio from './components/Relogio';
+import Home from './components/Home';
 
 export default function Routes() {
     return (
 
         <BrowserRouter>
             <Switch>
+
+                <Route exact path='/' component={Home} />
                 <Route path='/cronometro' component={Cronometro} />
                 <Route path='/relogio' component={Relogio} />
             </Switch>
