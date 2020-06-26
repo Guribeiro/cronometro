7ca0627306cf75c869e30716c5ecb42f7d490498
Gustavo Henrique <gugahribeiro@hotmail.com>
2020-06-26 17:08:22 -0300

    add home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component, Fragment } from 'react';
 import './styles.scss';
 
-class Home extends Component{
+import { IoIosTimer, IoIosClock } from 'react-icons/io'
 
-    render(){
-        return(
-            <div className='home'>
+import { Link } from 'react-router-dom';
 
-                <h1>Olá mundo</h1>
-            </div>
+
+
+class Home extends Component {
+
+
+
+
+    render() {
+
+
+        return (
+
+            <Fragment>
+
+                <div className='home'>
+                    
+                    <div className="options-service">
+                        <div className="option">
+                            <Link to='/relogio'>
+                                <IoIosClock />
+                                <strong>Relógio</strong>
+                            </Link>
+                        </div>
+
+                        <div className="option">
+                            <Link to='/cronometro'>
+                                <IoIosTimer />
+                                <strong>Cronômetro</strong>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
         )
     }
 }
